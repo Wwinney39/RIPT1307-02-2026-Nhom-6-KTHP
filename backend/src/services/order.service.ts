053eef4ced18907bc1a945a58e26efe60e0605bb
Wwinney39 <quanghoang392005@gmail.com>
2026-06-07 21:39:47 +0700

    fix: service.order

--- backend/src/services/order.service.ts
+++ backend/src/services/order.service.ts
@@ -187,10 +187,10 @@ export const orderService = {
     if (order[0].status.toUpperCase() !== 'PENDING') throw new Error('Chỉ có thể hủy đơn khi đang ở trạng thái PENDING!');
 
     await db.execute(
-        'UPDATE Orders SET status = "CANCELLED" WHERE order_id = ?', [orderId]
+        'UPDATE Orders SET status = ? WHERE order_id = ?', ['CANCELLED', orderId]
     );
     await db.execute(
-        'INSERT INTO delivery_Logs (order_id, status) VALUES (?, "Đã hủy")', [orderId]
+        'INSERT INTO delivery_Logs (order_id, status) VALUES (?, ?)', [orderId, 'Đã hủy']
     );
     },
 };
